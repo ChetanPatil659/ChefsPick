aaa4c8acefacf18e0d9aafbf663aaa609aa0f96b
Chetan Patil <patilchetan659@gmail.com>
2024-02-17 20:29:27 +0530

    db changes

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,11 @@ mongoose.set("strictQuery", false);
 const dbConnect = async () => {
   const PORT = "mongodb+srv://patilchetan659:FyJLnMF7HbsVnkoE@cluster0.o2qe1zg.mongodb.net/?retryWrites=true&w=majority"
   try {
-    await mongoose.connect(PORT);
+    await mongoose.connect({
+      PORT,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("Database Connected at", PORT);
   } catch (err) {
     console.error(err.message);
